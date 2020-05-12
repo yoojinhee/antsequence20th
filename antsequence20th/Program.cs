12738a3e8cfd2e761695f1cf1fc972e7dab9b9ed
yoojinhee <52348335+yhujinhui@users.noreply.github.com>
2020-05-12 21:26:03 +0900

    개미 수열 실습

--- antsequence20th/Program.cs
+++ antsequence20th/Program.cs
@@ -10,6 +10,33 @@ namespace antsequence20th
     {
         static void Main(string[] args)
         {
+            String s = "1";//12
+            String re;
+            char head;
+            for (int i=0; i<20; i++)
+            {
+                head = s[0];//1
+                int cnt = 1;
+                re = "";
+                for(int j=1; j<s.Length; j++)//2
+                {
+                    if (s[j].Equals(head))//
+                    {
+                        cnt++;//1
+                    }
+                    else
+                    {
+                        re += head;
+                        re += cnt;
+                        cnt = 1;
+                        head = s[j];
+                    }
+                }
+                re += head;//1
+                re += cnt;//1
+                s = re;
+                Console.WriteLine(s);
+            }
         }
     }
 }
